fa90a835f62611aa54fe3bdcfd8ded6a7bf40aa5
Dragonfore <matthewmackay26@hotmail.com>
2018-10-09 09:25:59 -0700

    Updated the view to include my code

--- twitterApp/views.py
+++ twitterApp/views.py
@@ -4,7 +4,9 @@ from django.urls import reverse
 from django.views import generic as gen
 from django.http import HttpResponse
 from .models import evaluation, Choice, Question
+import logging
 
+logger = logging.getLogger(__name__)
 
 class IndexView(gen.ListView):
     template_name = 'twitterApp/index.html'
@@ -58,6 +60,7 @@ def send_classifier_data(request):
     # request.META.get("HTTP_QUERYDATATYPE")
     currentQueryData = request.META.get("HTTP_QUERYDATA")
     currentQueryType = request.META.get("HTTP_QUERYDATATYPE")
+    logger.error(analyze_twitter_query(None))
     if currentQueryData == 'realdonaldtrump' and currentQueryType == 'username':
         processed_data = {'male': 164, 'positive': 45, 'female': 180, 'negative': 119}
     else:
@@ -65,17 +68,27 @@ def send_classifier_data(request):
     return HttpResponse(json.dumps(processed_data)) 
 
 def determine_gender(text_list):
-    return None
+    with open('static/bestClassifier.json') as file:
+        try:
+            g_classifier = file.read()
+            return g_classifier
+        except Exception as ex:
+            raise FileNotFoundError('Unable to load Classifier' + str(ex)) 
 
 def determine_sentiment(text_list):
-    return None
+    from nltk.sentiment.vader import SentimentIntensityAnalyzer
+    sentiment_list = []
+    SIA = SentimentIntensityAnalyzer()
+    for text in text_list:
+        sentiment_list.append(SIA.polarity_scores(text))
+    return sentiment_list
 
 def pull_twitter_data(queryData, queryDataType):
     import json
-    test_twiter_json = open('exampleTweet.json')
+    test_twiter_json = open('static/exampleTweet.json')
     test_json_object = json.load(test_twiter_json)
     test_twiter_json.close()
     return test_json_object
 
 def analyze_twitter_query(request):
-   return None 
+   return [str(determine_gender(["Something", "Something Else"])), str(determine_sentiment(['I hate', 'I love', 'I am neutral'])), str(pull_twitter_data(None, None))] 
