0ad6f76343de7489868d39e4a710d7ea92a2a7ad
Ubuntu <ubuntu@ip-172-31-20-21.us-west-2.compute.internal>
2018-10-16 18:21:03 +0000

    Modifiied WSGI

--- twitter_scraper/wsgi.py
+++ twitter_scraper/wsgi.py
@@ -8,9 +8,19 @@ https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
 
+path = 'home/ubuntu/twitterDemographics'
+
+if path not in sys.path:
+    sys.path.append(path)
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'twitter_scraper.settings')
 
-application = get_wsgi_application()
+import django.core.handlers.wsgi
+
+application = django.core.handlers.wsgi.WSGIHandler()
+
+#application = get_wsgi_application()
